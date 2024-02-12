f03eb03334161ad8a5276d65261e018e75dcaaf1
GlodBlock <1356392126@qq.com>
2024-02-12 14:15:59 +0800

    improve tag bus performance

--- src/main/java/com/glodblock/github/extendedae/common/me/taglist/TagPriorityList.java
+++ src/main/java/com/glodblock/github/extendedae/common/me/taglist/TagPriorityList.java
@@ -5,7 +5,11 @@ import appeng.util.prioritylist.IPartitionList;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
+import net.minecraft.core.Holder;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -19,10 +23,15 @@ public class TagPriorityList implements IPartitionList {
             new CacheLoader<>() {
                 @Override
                 public @NotNull Boolean load(@NotNull AEKey key) {
-                    for (var tag : tagKeys) {
-                        if (key.isTagged(tag)) {
-                            return true;
-                        }
+                    var obj = key.getPrimaryKey();
+                    Holder<?> refer = null;
+                    if (obj instanceof Item item) {
+                        refer = ForgeRegistries.ITEMS.getHolder(item).orElse(null);
+                    } else if (obj instanceof Fluid fluid) {
+                        refer = ForgeRegistries.FLUIDS.getHolder(fluid).orElse(null);
+                    }
+                    if (refer != null) {
+                        return refer.tags().anyMatch(tagKeys::contains);
                     }
                     return false;
                 }
