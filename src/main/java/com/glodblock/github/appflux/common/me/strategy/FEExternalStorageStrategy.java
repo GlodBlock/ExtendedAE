a3425a6a91402d7ced4a7cae849c120938e4a5f2
GlodBlock <1356392126@qq.com>
2024-04-03 18:35:22 +0800

    mek induction support

--- src/main/java/com/glodblock/github/appflux/common/me/strategy/FEExternalStorageStrategy.java
+++ src/main/java/com/glodblock/github/appflux/common/me/strategy/FEExternalStorageStrategy.java
@@ -12,12 +12,14 @@ import com.glodblock.github.appflux.common.me.key.FluxKey;
 import com.glodblock.github.appflux.common.me.key.type.EnergyType;
 import com.glodblock.github.appflux.common.me.key.type.FluxKeyType;
 import com.glodblock.github.appflux.util.AFUtil;
+import com.glodblock.github.appflux.xmod.mek.MekEnergy;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.energy.IEnergyStorage;
+import net.minecraftforge.fml.ModList;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("UnstableApiUsage")
@@ -25,31 +27,46 @@ public class FEExternalStorageStrategy implements ExternalStorageStrategy {
 
     private final Direction fromSide;
     private final BlockApiCache<IEnergyStorage> apiCache;
+    private final MekEnergy inductionHandler;
 
     public FEExternalStorageStrategy(ServerLevel level, BlockPos fromPos, Direction fromSide) {
         this.apiCache = BlockApiCache.create(ForgeCapabilities.ENERGY, level, fromPos);
         this.fromSide = fromSide;
+        if (ModList.get().isLoaded("mekanism")) {
+            this.inductionHandler = MekEnergy.INDUCTION.create(level, fromPos);
+        } else {
+            this.inductionHandler = MekEnergy.NULL.create(level, fromPos);
+        }
     }
 
     @Override
     public @Nullable MEStorage createWrapper(boolean extractableOnly, Runnable callback) {
         var storage = this.apiCache.find(this.fromSide);
-        if (storage == null) {
+        if (storage == null && !this.inductionHandler.valid()) {
             return null;
         }
-        return new FEStorageWrapper(storage, callback);
+        return new FEStorageWrapper(storage, this.inductionHandler, this.fromSide, callback);
     }
 
-    private record FEStorageWrapper(IEnergyStorage storage, Runnable callback) implements MEStorage {
+    private record FEStorageWrapper(@Nullable IEnergyStorage storage, MekEnergy inductionStorage, Direction side, Runnable callback) implements MEStorage {
 
         @Override
         public long insert(AEKey what, long amount, Actionable mode, IActionSource source) {
             if (FluxKey.of(EnergyType.FE).equals(what)) {
-                int in = this.storage.receiveEnergy(AFUtil.clampLong(amount), mode.isSimulate());
-                if (in > 0 && mode == Actionable.MODULATE) {
-                    this.callback.run();
+                if (this.inductionStorage.valid()) {
+                    long in = this.inductionStorage.input(amount, mode, this.side);
+                    if (in > 0 && mode == Actionable.MODULATE) {
+                        this.callback.run();
+                    }
+                    return in;
                 }
-                return in;
+                if (this.storage != null) {
+                    int in = this.storage.receiveEnergy(AFUtil.clampLong(amount), mode.isSimulate());
+                    if (in > 0 && mode == Actionable.MODULATE) {
+                        this.callback.run();
+                    }
+                    return in;
+                }
             }
             return 0;
         }
@@ -57,20 +74,36 @@ public class FEExternalStorageStrategy implements ExternalStorageStrategy {
         @Override
         public long extract(AEKey what, long amount, Actionable mode, IActionSource source) {
             if (FluxKey.of(EnergyType.FE).equals(what)) {
-                int out = this.storage.extractEnergy(AFUtil.clampLong(amount), mode.isSimulate());
-                if (out > 0 && mode == Actionable.MODULATE) {
-                    this.callback.run();
+                if (this.inductionStorage.valid()) {
+                    long out = this.inductionStorage.output(amount, mode, this.side);
+                    if (out > 0 && mode == Actionable.MODULATE) {
+                        this.callback.run();
+                    }
+                    return out;
                 }
-                return out;
+                if (this.storage != null) {
+                    int out = this.storage.extractEnergy(AFUtil.clampLong(amount), mode.isSimulate());
+                    if (out > 0 && mode == Actionable.MODULATE) {
+                        this.callback.run();
+                    }
+                    return out;
+                }
             }
             return 0;
         }
 
         @Override
         public void getAvailableStacks(KeyCounter out) {
-            int stored = this.storage.getEnergyStored();
-            if (stored > 0) {
-                out.add(FluxKey.of(EnergyType.FE), stored);
+            if (this.inductionStorage.valid()) {
+                long stored = this.inductionStorage.getStored();
+                if (stored > 0) {
+                    out.add(FluxKey.of(EnergyType.FE), stored);
+                }
+            } else if (this.storage != null) {
+                int stored = this.storage.getEnergyStored();
+                if (stored > 0) {
+                    out.add(FluxKey.of(EnergyType.FE), stored);
+                }
             }
         }
 
---
+++ src/main/java/com/glodblock/github/appflux/util/TileCache.java
@@ -0,0 +1,28 @@
+package com.glodblock.github.appflux.util;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+import javax.annotation.Nullable;
+
+public class TileCache {
+
+    private final ServerLevel level;
+    private final BlockPos fromPos;
+
+    private TileCache(ServerLevel level, BlockPos fromPos) {
+        this.level = level;
+        this.fromPos = fromPos;
+    }
+
+    public static TileCache create(ServerLevel level, BlockPos fromPos) {
+        return new TileCache(level, fromPos);
+    }
+
+    @Nullable
+    public BlockEntity find() {
+        return level.getBlockEntity(fromPos);
+    }
+
+}
