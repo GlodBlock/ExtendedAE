cff1a6bee9a01f853594428c5a5fc6964ba39bc3
GlodBlock <1356392126@qq.com>
2024-04-07 21:02:23 +0800

    fix wireless charger insert power to interface/pp

---
+++ src/main/java/com/glodblock/github/appflux/xmod/wirelesscharger/ChargerBlacklist.java
@@ -0,0 +1,22 @@
+package com.glodblock.github.appflux.xmod.wirelesscharger;
+
+import appeng.blockentity.networking.CableBusBlockEntity;
+import appeng.helpers.InterfaceLogicHost;
+import appeng.helpers.patternprovider.PatternProviderLogicHost;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ChargerBlacklist {
+
+    public final static List<Predicate<BlockEntity>> BLACKLIST = new ArrayList<>();
+
+    static {
+        BLACKLIST.add(te -> te instanceof PatternProviderLogicHost);
+        BLACKLIST.add(te -> te instanceof InterfaceLogicHost);
+        BLACKLIST.add(te -> te instanceof CableBusBlockEntity);
+    }
+
+}
