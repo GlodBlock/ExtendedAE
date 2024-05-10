9fe8d27ab65f420b56d5370c15ea140dee77915b
GlodBlock <1356392126@qq.com>
2024-05-10 13:43:55 +0800

    bump version

--- src/main/java/com/glodblock/github/extendedae/xmod/appflux/AFCommonLoad.java
+++ src/main/java/com/glodblock/github/extendedae/xmod/appflux/AFCommonLoad.java
@@ -12,10 +12,10 @@ public class AFCommonLoad {
     public static void init() {
         try {
             ChargerBlacklist.BLACKLIST.add(te -> te instanceof TileIngredientBuffer);
-            Upgrades.add(AFItemAndBlock.INDUCTION_CARD, EPPItemAndBlock.EX_INTERFACE, 1, GuiText.Interface.getTranslationKey());
-            Upgrades.add(AFItemAndBlock.INDUCTION_CARD, EPPItemAndBlock.EX_INTERFACE_PART, 1, GuiText.Interface.getTranslationKey());
-            Upgrades.add(AFItemAndBlock.INDUCTION_CARD, EPPItemAndBlock.EX_PATTERN_PROVIDER, 1, "group.pattern_provider.name");
-            Upgrades.add(AFItemAndBlock.INDUCTION_CARD, EPPItemAndBlock.EX_PATTERN_PROVIDER_PART, 1, "group.pattern_provider.name");
+            Upgrades.add(AFItemAndBlock.INDUCTION_CARD, EPPItemAndBlock.EX_INTERFACE, 1, "gui.expatternprovider.ex_interface");
+            Upgrades.add(AFItemAndBlock.INDUCTION_CARD, EPPItemAndBlock.EX_INTERFACE_PART, 1, "gui.expatternprovider.ex_interface");
+            Upgrades.add(AFItemAndBlock.INDUCTION_CARD, EPPItemAndBlock.EX_PATTERN_PROVIDER, 1, "gui.expatternprovider.ex_pattern_provider");
+            Upgrades.add(AFItemAndBlock.INDUCTION_CARD, EPPItemAndBlock.EX_PATTERN_PROVIDER_PART, 1, "gui.expatternprovider.ex_pattern_provider");
         } catch (Throwable ignored) {
             // NO-OP
         }
