a831286d9a00a6d081eaab2e84313b709e662462
GilbertzRivii <tomek.handerek@gmail.com>
2025-08-19 12:59:08 +0200

    Used ae2 widget container instead

--- src/main/java/com/glodblock/github/extendedae/client/gui/GuiTagStorageBus.java
+++ src/main/java/com/glodblock/github/extendedae/client/gui/GuiTagStorageBus.java
@@ -59,19 +59,28 @@ public class GuiTagStorageBus extends UpgradeableScreen<ContainerTagStorageBus> 
             if (this.filterInputs2 != null) this.filterInputs2.setValue(o.get(1));
         });
 
-        EPPNetworkHandler.INSTANCE.sendToServer(new CGenericPacket("update"));
-    }
-
-    private MultilineTextFieldWidget createMultiline(int x, int y, int w, int h, boolean first) {
-        Font font = Minecraft.getInstance().font;
         var placeholder = Component.translatable("gui.expatternprovider.tag_storage_bus.tooltip");
-        var tf = new MultilineTextFieldWidget(font, x, y, w, h, placeholder);
-        tf.setFilter(ORE_DICTIONARY_FILTER);
-        tf.setMaxLength(512);
-        tf.setResponder(s ->
-                EPPNetworkHandler.INSTANCE.sendToServer(new CGenericPacket("set", s, first))
-        );
-        return tf;
+
+        this.filterInputs = new MultilineTextFieldWidget(
+                Minecraft.getInstance().font, 0, 0, 160, 26, placeholder);
+        this.filterInputs.setFilter(ORE_DICTIONARY_FILTER);
+        this.filterInputs.setMaxLength(1024);
+        this.filterInputs.setResponder(s ->
+                EPPNetworkHandler.INSTANCE.sendToServer(new CGenericPacket("set", s, true)));
+
+        this.filterInputs2 = new MultilineTextFieldWidget(
+                Minecraft.getInstance().font, 0, 0, 160, 26, placeholder);
+        this.filterInputs2.setFilter(ORE_DICTIONARY_FILTER);
+        this.filterInputs2.setMaxLength(1024);
+        this.filterInputs2.setResponder(s ->
+                EPPNetworkHandler.INSTANCE.sendToServer(new CGenericPacket("set", s, false)));
+
+        widgets.add("filter_input", this.filterInputs);
+        widgets.add("filter_input_2", this.filterInputs2);
+
+        setInitialFocus(this.filterInputs);
+
+        EPPNetworkHandler.INSTANCE.sendToServer(new CGenericPacket("update"));
     }
 
     @Override
@@ -96,27 +105,6 @@ public class GuiTagStorageBus extends UpgradeableScreen<ContainerTagStorageBus> 
             guiGraphics.drawString(font, GuiText.Unattached.text(), 0, 0, color.toARGB(), false);
         }
         poseStack.popPose();
-    }
-
-    @Override
-    protected void init() {
-        super.init();
-
-        if (this.filterInputs == null || this.filterInputs2 == null) {
-            var a = style.getWidget("filter_input");
-            var b = style.getWidget("filter_input_2");
-
-            int ax = a.getLeft(), ay = a.getTop(), aw = a.getWidth(), ah = a.getHeight();
-            int bx = b.getLeft(), by = b.getTop(), bw = b.getWidth(), bh = b.getHeight();
-
-            this.filterInputs  = createMultiline(this.getGuiLeft() + ax, this.getGuiTop() + ay, aw, ah, true);
-            this.filterInputs2 = createMultiline(this.getGuiLeft() + bx, this.getGuiTop() + by, bw, bh, false);
-
-            addRenderableWidget(this.filterInputs);
-            addRenderableWidget(this.filterInputs2);
-        }
-
-        setInitialFocus(this.filterInputs);
     }
 
     @NotNull
