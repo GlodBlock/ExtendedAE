ed192e789a57f026b72e209c6f690280006c6fe3
Kolja <104643407+Thatguyello@users.noreply.github.com>
2025-11-20 00:41:22 +0100

    remove unused settings class

--- src/main/java/com/glodblock/github/extendedae/api/EPPSettings.java
+++
@@ -1,7 +0,0 @@
-package com.glodblock.github.extendedae.api;
-
-import appeng.api.config.Setting;
-
-public class EPPSettings {
-    public static final Setting<StorageMode> STORAGE_MODE = new Setting<>("storage_mode", StorageMode.class);
-}
