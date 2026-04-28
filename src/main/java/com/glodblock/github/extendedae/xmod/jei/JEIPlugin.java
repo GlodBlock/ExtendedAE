c303e33b8836813253d86202855cdcba4b83eff3
GlodBlock <1356392126@qq.com>
2026-04-28 19:07:39 +0800

    add generic stack recipe lookup in pattern gui

--- src/main/java/com/glodblock/github/extendedae/xmod/jei/JEIPlugin.java
+++ src/main/java/com/glodblock/github/extendedae/xmod/jei/JEIPlugin.java
@@ -1,10 +1,16 @@
 package com.glodblock.github.extendedae.xmod.jei;
 
+import appeng.api.stacks.AEItemKey;
+import appeng.api.stacks.GenericStack;
+import appeng.client.gui.StackWithBounds;
+import appeng.items.misc.WrappedGenericStack;
 import com.glodblock.github.extendedae.ExtendedAE;
 import com.glodblock.github.extendedae.client.gui.GuiCircuitCutter;
 import com.glodblock.github.extendedae.client.gui.GuiCrystalAssembler;
 import com.glodblock.github.extendedae.client.gui.GuiExInscriber;
+import com.glodblock.github.extendedae.client.gui.pattern.GuiPattern;
 import com.glodblock.github.extendedae.common.EAESingletons;
+import com.glodblock.github.extendedae.container.pattern.ContainerPattern;
 import com.glodblock.github.extendedae.recipe.CircuitCutterRecipe;
 import com.glodblock.github.extendedae.recipe.CrystalAssemblerRecipe;
 import com.glodblock.github.extendedae.recipe.CrystalFixerRecipe;
@@ -21,7 +27,10 @@ import mezz.jei.api.registration.IGuiHandlerRegistration;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeCategoryRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.runtime.IClickableIngredient;
+import mezz.jei.api.runtime.IJeiRuntime;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.Rect2i;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Recipe;
@@ -30,14 +39,23 @@ import net.minecraft.world.item.crafting.RecipeInput;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeType;
 import org.jetbrains.annotations.NotNull;
+import tamaized.ae2jeiintegration.integration.modules.jei.GenericEntryStackHelper;
 import tamaized.ae2jeiintegration.integration.modules.jei.categories.ChargerCategory;
 import tamaized.ae2jeiintegration.integration.modules.jei.categories.InscriberRecipeCategory;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 @JeiPlugin
 public class JEIPlugin implements IModPlugin {
+
+    private IJeiRuntime jeiRuntime;
+
+    @Override
+    public void onRuntimeAvailable(@NotNull IJeiRuntime jeiRuntime) {
+        this.jeiRuntime = jeiRuntime;
+    }
 
     @Override
     public @NotNull ResourceLocation getPluginUid() {
@@ -98,6 +116,35 @@ public class JEIPlugin implements IModPlugin {
                         @Override
                         public @NotNull Collection<IGuiClickableArea> getGuiClickableAreas(@NotNull GuiExInscriber screen, double mouseX, double mouseY) {
                             return List.of(IGuiClickableArea.createBasic(82, 50, 26, 16, InscriberRecipeCategory.RECIPE_TYPE));
+                        }
+                    }
+            );
+            registry.addGenericGuiContainerHandler(GuiPattern.class,
+                    new IGuiContainerHandler<GuiPattern<?>>() {
+                        @Override
+                        public @NotNull Optional<IClickableIngredient<?>> getClickableIngredientUnderMouse(@NotNull GuiPattern<?> screen, double mouseX, double mouseY) {
+                            var stackWithBounds = screen.getSlotUnderMouse();
+                            if (stackWithBounds instanceof ContainerPattern.DisplayOnlySlot dpSlot) {
+                                var genStack = dpSlot.getItem();
+                                if (!genStack.isEmpty()) {
+                                    var item = genStack.getItem();
+                                    var key = item instanceof WrappedGenericStack wgs
+                                            ? wgs.unwrapWhat(genStack) : AEItemKey.of(genStack);
+                                    var amount = item instanceof WrappedGenericStack wgs
+                                            ? wgs.unwrapAmount(genStack) : dpSlot.getActualAmount();
+                                    if (key != null && amount > 0) {
+                                        var area = new Rect2i(screen.getGuiLeft() + dpSlot.x, screen.getGuiTop() + dpSlot.y, 16, 16);
+                                        var ing = GenericEntryStackHelper.stackToClickableIngredient(
+                                                jeiRuntime.getIngredientManager(),
+                                                new StackWithBounds(new GenericStack(key, amount), area)
+                                        );
+                                        if (ing != null) {
+                                            return Optional.of(ing);
+                                        }
+                                    }
+                                }
+                            }
+                            return Optional.empty();
                         }
                     }
             );
