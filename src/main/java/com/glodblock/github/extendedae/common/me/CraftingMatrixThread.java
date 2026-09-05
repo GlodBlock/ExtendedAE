3b2acffe0d8143cdfbd1dd9f52368f37fb079764
GlodBlock <1356392126@qq.com>
2026-09-05 16:46:47 +0800

    fix valid check of matrix cache

--- src/main/java/com/glodblock/github/extendedae/common/me/CraftingMatrixThread.java
+++ src/main/java/com/glodblock/github/extendedae/common/me/CraftingMatrixThread.java
@@ -183,7 +183,7 @@ public class CraftingMatrixThread extends CraftingThread {
         static SearchResult NULL = new SearchResult(null, null, null, null, 0);
 
         boolean isValid() {
-            return this != NULL || this.results != null;
+            return this != NULL && this.results != null;
         }
 
     }
